2c2b3a6fb90c5ff7ea6abfd4be37b4ae9e845c0e
yie <yie@live.cn>
2018-12-06 15:00:03 +0800

    first commit

---
+++ e/data/adminlogin/user1_0a7174956c823df98ad773178b0c3865.php
@@ -0,0 +1,16 @@
+<?php
+define('EcmsAdminLogin','EmpireCMS');
+define('EcmsAdminLoginDEF','dg7c7b6963bdd203500bdb9372600c704a');
+$ecms_adminloginr=array();
+$ecms_adminloginr=Array('userid'=>'1',
+'ernd'=>'sPT9qasdTzk6PkkHW2nKASlw4kn',
+'erndtwo'=>'IA21hchbaFUKrjpCqatw',
+'erndadd'=>'xnhHuGKWElJmJDm1m27ZcoBLL9sirRPG',
+'ehash'=>'neGstt22hZQevIBuVPL4',
+'ehashname'=>'ehash_7FVtc',
+'rhash'=>'OKEPTWxnlGjP',
+'rhashname'=>'rhash_uYkyf',
+'eckdiyvar'=>'emecjjXpOFAM',
+'eckdiyval'=>'uGg1unevwpmqbjL03H',
+'edbdata'=>'1');
+?>
